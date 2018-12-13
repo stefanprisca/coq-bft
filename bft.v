94a3bf92b7386bada7d01f6c74cd64250b9fd370
Stefan Prisca <stefan.prisca@gmail.com>
2018-12-13 15:18:11 +0100

    Add NonFaulty replicas with working examples

--- bft.v
+++ bft.v
@@ -8,29 +8,92 @@ Require Import String.
 Require Import Maps.
 Open Scope string_scope.
 
+
+Definition FNum := 2.
+Definition QuorumAcceptanceThreshold := 2*FNum + 1.
+Definition MaxMessages := 1000.
+
+
 Definition ReplicaId := string.
 Definition SysView := string.
 
 Inductive Message : Type := 
-| message: ReplicaId -> string -> Message.
+| reqMsg: string -> ReplicaId -> Message
+| pPrepMsg: string -> nat -> ReplicaId -> Message
+| prepMsg: string -> nat -> ReplicaId -> Message.
 
-Notation "<< rId , pLoad >>" := (message rId pLoad).
+Notation "<<REQ digest , rId >>" := (reqMsg digest rId).
+Notation "<<PPREP digest , seqNum , rId >>" := (pPrepMsg digest seqNum rId).
+Notation "<<PREP digest , seqNum , rId >>" := (prepMsg digest seqNum rId).
 
 
-Definition get_rId (msg : Message) : ReplicaId :=
+Definition GetRID (msg : Message) : ReplicaId :=
   match msg with
-  | message x y => x
+  | reqMsg digest rId => rId
+  | pPrepMsg digest seqNum rId => rId
+  | prepMsg digest seqNum rId => rId
   end.
 
-Definition get_pLoad (msg : Message) : string :=
+Definition GetDigest (msg : Message) : string :=
   match msg with
-  | message x y => y
+  | reqMsg digest rId => digest
+  | pPrepMsg digest seqNum rId => digest
+  | prepMsg digest seqNum rId => digest
   end.
 
-Definition Ledger := total_map nat.
+Definition Ledger := list (string * nat).
+Definition EmptyLedger : Ledger :=  nil.
+
+Fixpoint beq_ledgers (l1 l2 : Ledger) :=
+match l1 with
+| nil => match l2 with
+          | nil => true
+          | _ => false
+         end
+| (s1, n1)::t => match l2 with
+          | nil => false
+          | (s2, n2)::t2 => andb (andb (beq_string s1 s2) (beq_nat n1 n2)) (beq_ledgers t t2)
+         end
+end.
+
 Definition SysState := total_map Ledger.
 Definition MessageBox := list Message.
-Definition Replica := SysState -> Message -> MessageBox -> SysState.
+
+
+Inductive QCert : Type :=
+| qCert : string -> nat -> QCert.
+
+Definition QCerts := total_map (list QCert).
+
+Definition EmptyQCerts : QCerts := fun _ => nil.
+
+Inductive ExecEnvironment : Type :=
+| execEnv : list ReplicaId -> SysState -> QCerts -> MessageBox -> ExecEnvironment.
+
+
+Definition GetEnvState (env : ExecEnvironment) := 
+  match env with
+  | execEnv _ state _ _  => state
+  end.
+
+Definition GetEnvReplicas (env : ExecEnvironment) := 
+  match env with
+  | execEnv replicas _ _ _  => replicas
+  end.
+
+
+Definition GetEnvMsgBox (env : ExecEnvironment) := 
+  match env with
+  | execEnv _ _ _  mb => mb
+  end.
+
+Definition GetEnvQCerts (env : ExecEnvironment) := 
+  match env with
+  | execEnv _ _ qcs _ => qcs
+  end.
+
+Definition Replica := ExecEnvironment -> Message -> ExecEnvironment.
+
 Definition SysReplicas := list (ReplicaId * Replica).
 
 Inductive System : Type := 
@@ -45,6 +108,18 @@ Definition GetState (sys : System) :=
   match sys with
   | system _ state _ => state
   end.
+
+Fixpoint getReplicaIdsHelper (replicas : SysReplicas) (result : list ReplicaId) :=
+  match replicas with
+  | nil => result
+  | (rId, _)::t => rId::(getReplicaIdsHelper t result)
+  end.
+
+Definition GetReplicaIds (sys : System) := 
+  match sys with
+  | system _ _ replicas => (getReplicaIdsHelper replicas nil)
+  end.
+
 
 Definition GetReplicas (sys : System) := 
   match sys with
@@ -63,46 +138,125 @@ let replicas := GetReplicas sys in
   findReplicaHelper replicas rId.
 
 
-Definition empty_ledger : Ledger :=  (fun _ => 0).
-Definition empty_state : SysState :=  (fun _ => empty_ledger).
-Definition empty_replicas : SysReplicas :=  nil.
+Definition EmptyState : SysState :=  (fun _ => EmptyLedger).
+Definition EmptyReplicas : SysReplicas :=  nil.
 
-Definition ProcessMessage (sys:System) (rId : ReplicaId) (msg:Message) (msgBox : MessageBox) :=
- match (FindReplica sys rId) with
-    | None => sys
-    | Some replica =>
-      let sysState' := (replica (GetState sys) msg msgBox) in
-        (system "foo" sysState' (GetReplicas sys))
+Fixpoint multicast (from : ReplicaId) (replicas : list ReplicaId) (msgTo : ReplicaId -> Message) (mBox : MessageBox) : MessageBox :=
+  match replicas with
+  | nil => mBox
+  | rId::t => if (beq_string rId from) then multicast from t msgTo mBox
+              else (msgTo rId)::(multicast from t msgTo mBox)
   end.
 
-Fixpoint ProcessAllMessages (sys:System) (msgBox: MessageBox) : System := 
-  match msgBox with
-  | nil => sys
-  | msg::t => let rId := get_rId msg in
-              let sys' := ProcessMessage sys rId msg msgBox in
-                ProcessAllMessages sys' t
-  end
-.
+Definition pushCertificate (qCerts : QCerts) (rId : ReplicaId) (qCert : QCert) : QCerts :=
+  qCerts & {rId --> qCert::(qCerts rId)}.
 
-Definition Request := string.
+Fixpoint seqNumAccepted (l:Ledger) (n:nat) :=
+    match l with
+    | nil => false
+    | (_, v)::t => orb (beq_nat n v) (seqNumAccepted t n)
+    end.
 
-Definition ProcessRequest (sys : System) (req : Request) :=
-  ProcessAllMessages sys [<<"foo", req>>].
+Fixpoint hasEnoughQCerts (qCertL : list QCert) (digest : string) (seqNum collected : nat) :=
+    match qCertL with
+    | nil => beq_nat collected QuorumAcceptanceThreshold
+    | (qCert d v)::t => if (andb (beq_string digest d) (beq_nat v seqNum)) then
+                           hasEnoughQCerts t digest seqNum (S collected)
+                        else
+                            hasEnoughQCerts t digest seqNum collected
+    end.
 
+Definition isQuorumCertified (qCertsL : list QCert) (digest : string) (seqNum : nat):=
+    hasEnoughQCerts qCertsL digest seqNum 0.
+
+Definition pushSequenceNum (st : SysState) (rId : ReplicaId) (digest : string) (seqNum : nat) :=
+    st & {rId --> (digest, seqNum)::(st rId)}.
+
+Definition genSeqNum (l : Ledger) :=
+    match l with
+      | nil => 0
+      | (d, v)::t => S v
+    end.
+
+Definition NFReplica : Replica := 
+  fun env msg => 
+      match msg with
+      | (reqMsg digest rId) => let n := (genSeqNum ((GetEnvState env) rId)) in
+                                let mb' := (multicast rId (GetEnvReplicas env) (pPrepMsg digest n) (GetEnvMsgBox env)) in
+                                  let qCerts' := (pushCertificate (GetEnvQCerts env) rId (qCert digest n)) in
+                                    execEnv (GetEnvReplicas env) (GetEnvState env) qCerts' mb'
+      | (pPrepMsg digest n rId) => if (seqNumAccepted ((GetEnvState env) rId) n) then
+                                        env
+                                   else
+                                      let mb' := (multicast rId (GetEnvReplicas env) (prepMsg digest n) (GetEnvMsgBox env)) in
+                                      let qCerts' := (pushCertificate (GetEnvQCerts env) rId (qCert digest n)) in
+                                        if (isQuorumCertified (qCerts' rId) digest n) then
+                                           let state' := (pushSequenceNum (GetEnvState env) rId digest n) in
+                                              execEnv (GetEnvReplicas env) state' qCerts' mb'
+                                        else
+                                              execEnv (GetEnvReplicas env) (GetEnvState env) qCerts' mb'
+      | (prepMsg digest n rId) => if (seqNumAccepted ((GetEnvState env) rId) n) then
+                                        env
+                                   else
+                                      let qCerts' := (pushCertificate (GetEnvQCerts env) rId (qCert digest n)) in
+                                        if (isQuorumCertified (qCerts' rId) digest n) then
+                                           let state' := (pushSequenceNum (GetEnvState env) rId digest n) in
+                                              execEnv (GetEnvReplicas env) state' qCerts' (GetEnvMsgBox env)
+                                        else
+                                              execEnv (GetEnvReplicas env) (GetEnvState env) qCerts' (GetEnvMsgBox env)
+    end.
+
+Definition ProcessMessage (msg:Message) (sys:System) (rId : ReplicaId) (env : ExecEnvironment) :=
+ match (FindReplica sys rId) with
+    | None => env
+    | Some replica => (replica env msg)
+  end.
+
+Fixpoint ProcessAllMessages (fuel : nat) (sys:System) (env : ExecEnvironment) := 
+  match fuel with
+  | 0 => env
+  | S f' =>
+      match (GetEnvMsgBox env) with
+      | nil => env
+      | msg::t => let rId := GetRID msg in
+                  let envI := (execEnv (GetEnvReplicas env) (GetEnvState env) (GetEnvQCerts env) t) in
+                  let env' := ProcessMessage msg sys rId envI in
+                    ProcessAllMessages f' sys env'
+      end
+  end.
+
+Print ExecEnvironment.
+Print System.
+Definition ProcessRequest (sys : System) (req : Message) : System :=
+  let env' := ProcessAllMessages MaxMessages sys (execEnv (GetReplicaIds sys) (GetState sys) EmptyQCerts [req]) in
+    (system "" (GetEnvState env') (GetReplicas sys)).
+
+Fixpoint getCommonLedger (state : SysState) (replicas: SysReplicas) (gLedger : Ledger) :=
+match replicas with
+    | nil => Some gLedger
+    | (rId, _)::t => if (beq_ledgers (state rId) gLedger) then (getCommonLedger state t gLedger)
+                      else None
+end.
+
+Definition GetGlobalLedger (sys:System) : option Ledger :=
+  let state := GetState sys in
+    let gLedger := state "primary" in
+      let replicas := GetReplicas sys in
+         getCommonLedger state replicas gLedger.
+(*
 Definition NonFaulty (rId : ReplicaId) : Prop := True.
 Definition MajorityNonFaulty (sys:System) : Prop := True.
 
 Inductive StateValid : System -> Prop :=
-| empty_valid : forall sys:System, (GetState sys) = empty_state 
+| empty_valid : forall sys:System, (GetState sys) = EmptyState 
                   -> StateValid sys.
 
 
-Lemma system_safety: forall (sys:System) (msgBox:MessageBox),
+Lemma system_safety: forall (sys:System),
   MajorityNonFaulty sys
-  -> StateValid (ProcessAllMessages sys msgBox)
-  -> forall (m:Message), StateValid (ProcessAllMessages sys (m::msgBox)).
+  -> StateValid sys
+  -> forall (r:Request), StateValid (ProcessRequest sys r).
 Proof. Admitted.
-
 
 Theorem byzantine_fault_tolerant : forall (sys:System) (msgBox : MessageBox),
     MajorityNonFaulty sys 
@@ -114,5 +268,5 @@ induction msgBox.
   - induction replicas.
     + apply IHmsgBox.
     + apply system_safety; assumption.
-Qed.
+Qed.*)
 
--- example-tests.v
+++ example-tests.v
@@ -3,39 +3,54 @@ Require Import Maps.
 Require Import Coq.Lists.List.
 Import ListNotations.
 
-Definition one_replicas : SysReplicas := ("foo", fun _ _ _ => empty_state)::empty_replicas.
-Definition system1 := (system "foo" empty_state one_replicas).
-Definition m1 := <<"foo", "1">>.
+Definition replicas1 : SysReplicas := ("foo", fun env _ => env)::EmptyReplicas.
+Definition system1 := (system "foo" EmptyState replicas1).
+Definition m1 := <<REQ "foo", "1">>.
 Definition twoMsgBox := [m1 ; m1].
+Print ExecEnvironment.
+Definition execEnv1 := (execEnv (GetReplicaIds system1) EmptyState EmptyQCerts nil).
 
-Example sysEmptyTest : GetState (ProcessAllMessages system1 twoMsgBox) = empty_state.
+Example sysEmptyTest : GetEnvState (ProcessAllMessages MaxMessages system1 execEnv1) = EmptyState.
 Proof. reflexivity. Qed.
 
-Example stValid_ProcMessages_sys1 : StateValid (ProcessAllMessages system1 twoMsgBox).
-Proof. constructor. reflexivity.   Qed.
+Definition activeReplicas1 : SysReplicas :=  
+      ("foo" , NFReplica )
+      ::EmptyReplicas.
+Definition activeSystem1 := (system "" EmptyState activeReplicas1).
+Definition activeEnv1 := (execEnv (GetReplicaIds activeSystem1) (EmptyState) EmptyQCerts [<<REQ "1", "foo"  >> ]).
 
-Print Replica.
-Print Ledger.
+Compute (GetEnvState (ProcessAllMessages MaxMessages activeSystem1 activeEnv1)) "foo".
+Example activeSystem1_noState : (GetEnvState (ProcessAllMessages MaxMessages activeSystem1 activeEnv1)) = EmptyState.
+Proof. reflexivity. Qed.
 
-Definition active_replicas : SysReplicas :=  
-      ("foo" , fun sysSt msg mb => sysSt & {"foo" --> (empty_ledger & { (get_pLoad msg) --> 1 }) } )
-      ::empty_replicas.
-Definition systemActive := (system "foo" empty_state active_replicas).
-Example stValid_ProcMessages_sysActive : StateValid (ProcessAllMessages systemActive twoMsgBox).
-Proof.  repeat apply system_safety; constructor; reflexivity.
+Compute (GetEnvQCerts (ProcessAllMessages MaxMessages activeSystem1 activeEnv1)) "foo".
+Example activeSystem1_oneCertificate : length ((GetEnvQCerts (ProcessAllMessages MaxMessages activeSystem1 activeEnv1)) "foo") = 1.
+Proof. reflexivity. Qed.
+
+Compute (GetEnvMsgBox (ProcessAllMessages MaxMessages activeSystem1 activeEnv1)).
+Example activeSystem1_emptyMessages : length (GetEnvMsgBox (ProcessAllMessages MaxMessages activeSystem1 activeEnv1)) = 0.
+Proof. reflexivity. Qed.
+
+Definition multipleActiveReplicas : SysReplicas :=
+      ("foo" , NFReplica )::("foo1" , NFReplica )::("foo2" , NFReplica )::("primary" , NFReplica )::("foo4" , NFReplica )
+      ::("foo5" , NFReplica )::("foo6" , NFReplica )::("foo7" , NFReplica )
+      ::("foo9" , NFReplica )::("foo8" , NFReplica )::EmptyReplicas.
+
+Definition multiReplicaSys := (system "" EmptyState multipleActiveReplicas).
+Definition multiReplicaEnv := (execEnv (GetReplicaIds multiReplicaSys) (EmptyState) EmptyQCerts [<<REQ "digest", "primary" >>]).
+
+Compute (GetEnvState (ProcessAllMessages MaxMessages multiReplicaSys multiReplicaEnv)) "foo5".
+Compute (GetState (ProcessRequest multiReplicaSys <<REQ "digest", "primary" >>)) "foo".
+
+Example procRequest_multiReplicaSys : (GetState (ProcessRequest multiReplicaSys <<REQ "digest", "primary" >>) ) "primary" = [("digest", 0)].
+Proof. reflexivity. Qed.
+
+Definition processedSys := ProcessRequest multiReplicaSys <<REQ "digest", "primary" >>.
+
+Example globalLedgerConsistent : let sys' := (ProcessRequest multiReplicaSys <<REQ "digest", "primary" >>) in 
+                                  GetGlobalLedger sys' =  Some ((GetState sys') "primary").
+Proof. intros. reflexivity. 
 Qed.
 
-Definition multiple_active_replicas : SysReplicas :=
-      ("foo" , fun sysSt msg mb => sysSt & {"foo" --> (empty_ledger & { (get_pLoad msg) --> 1 }) } )
-      :: ("bar" , fun sysSt msg mb => sysSt & {"bar" --> (empty_ledger & { (get_pLoad msg) --> 1 }) } )
-      :: ("baz" , fun sysSt msg mb => sysSt & {"baz" --> (empty_ledger & { (get_pLoad msg) --> 1 }) } )
-      ::empty_replicas.
-
-Definition multi_replica_sys := (system "" empty_state multiple_active_replicas).
-Definition multi_replica_msgBox := [<<"foo", "1">> ; <<"bar" , "1">>; <<"baz" , "1">>].
-
-Example stValid_ProcMessages_multi_replicas : StateValid (ProcessAllMessages multi_replica_sys multi_replica_msgBox).
-Proof.  repeat apply system_safety; constructor; reflexivity.
-Qed.
 
 
