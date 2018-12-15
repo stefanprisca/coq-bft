fa20d76f692eec0ea73a8a1a58f9e09e9b9f4148
Stefan Prisca <stefan.prisca@gmail.com>
2018-12-15 22:10:12 +0100

    Add some comments

--- bdf2.v
+++ bdf2.v
@@ -14,95 +14,123 @@ Inductive Replica : Set :=
 | FaultyReplica : Replica
 | NFReplica : Replica.
 
-Definition State := list (Replica * nat).
-
 Definition IsNonFaulty (r:Replica) :  Prop :=
   match r with 
     | FaultyReplica => False
     | NFReplica => True
   end.
 
+Print list.
+
+Inductive State : Type :=
+| prePrep: (Replica * nat) -> State
+| prep : (Replica * nat) -> State -> State.
+
 Definition ReplicaFun := State -> string -> State.
 
-Definition nfrValue := 1.
-Definition NFReplicaFun (st:State) (msg:string):=
+Definition nfrValue := 0.
+Definition faultyValue := 100.
+
+Fixpoint getPrimaryValue (st:State) : nat :=
+    match st with
+    | prePrep (P, n) => n
+    | prep _ t => getPrimaryValue t
+    end.
+
+Definition NFReplicaFun (st:State) (req:string):=
       match st with
-      | nil => [(NFReplica, nfrValue)]
-      | (h, d)::t => (NFReplica, d)::(h, d)::t
+      | prePrep (P, n) => prep (NFReplica, n) (prePrep (P, n))
+      | st => prep (NFReplica, (getPrimaryValue st)) st
       end
 .
 
 Definition GetReplicaFun (r:Replica) : ReplicaFun :=
   match r with
-  | FaultyReplica => fun st m => st
+  | FaultyReplica => fun st m => prep (FaultyReplica, faultyValue) st
   | NFReplica => NFReplicaFun
   end.
 
 
-Definition System := list Replica.
+Inductive System : Type :=
+| primary : Replica -> System
+| replica : Replica -> System -> System.
 
 
-Fixpoint ProcessRequest (sys : System) (m : string) (st : State) : State :=
+Fixpoint ProcessRequest (sys : System) (req : string) : State :=
   match sys with
-  | nil => st
-  | r::t => ((GetReplicaFun r) (ProcessRequest t m st) m) 
+  | primary P => prePrep (P, nfrValue)
+  | replica r sys' => ((GetReplicaFun r) (ProcessRequest sys' req) req) 
   end.
 
-Lemma NFR_appends_expected_state: forall r:Replica, IsNonFaulty r
+
+Lemma getPrimaryValue_nfrValue: forall (sys:System) (req: string),
+      getPrimaryValue (ProcessRequest sys req) = nfrValue.
+Proof. intros. induction sys.
+  - reflexivity.
+  - remember (ProcessRequest sys req) as st. 
+    simpl. rewrite <- Heqst. induction st;
+      destruct p; induction r; simpl; simpl in IHsys; apply IHsys.
+Qed.
+
+(* TODO: NFR state should be quorum value. *)
+Lemma NFR_state_nfrValue: forall r:Replica, IsNonFaulty r
          -> forall (sys:System) (req:string), 
-              ProcessRequest (r::sys) req nil 
-               = ((r, nfrValue)::(ProcessRequest sys req nil)).
+              ProcessRequest (replica r sys) req 
+               = (prep (r, nfrValue) (ProcessRequest sys req)).
 Proof. intros.
   generalize dependent sys. induction r.
   - inversion H.
   - simpl. intros. induction sys.
     + simpl. reflexivity.
-    + destruct a.
-      { simpl. apply IHsys. }
-      { simpl. rewrite IHsys. reflexivity. }
-
+    + induction r.
+      { simpl. rewrite getPrimaryValue_nfrValue. reflexivity. }
+      { simpl. rewrite IHsys. simpl. rewrite getPrimaryValue_nfrValue. reflexivity. }
 Qed. 
 
+Definition getLedgerHelper (l:nat) (ledger : option nat) :=
+match ledger with
+| Some l => Some l
+| _ => None
+end.
 
-Definition dumb1 := [NFReplica; NFReplica; FaultyReplica; NFReplica].
-Compute ProcessRequest dumb1 "bla" nil.
-
-Fixpoint GetLedger (st:State) (d: nat) : option nat :=
+Fixpoint GetLedger (st:State) : option nat :=
 match st with
-| nil => None
-| (NFReplica, l)::t => if (beq_nat d l) then (GetLedger t d)
-                        else None
-| (FaultyReplica, _)::t => GetLedger t d
+| prePrep (P, n) => Some n
+| prep (NFReplica, l) t => getLedgerHelper l (GetLedger t)
+| prep (FaultyReplica, l) t => GetLedger t
 end.
 
 Lemma NFR_maintains_ledger:  
         forall (sys:System) (req:string) (r:Replica) , 
             IsNonFaulty r
-         -> (GetLedger (ProcessRequest sys req nil) nfrValue) 
-            = (GetLedger (ProcessRequest (r::sys) req nil) nfrValue).
-Proof. intros. rewrite NFR_appends_expected_state.
+         -> (GetLedger (ProcessRequest sys req)) 
+            = (GetLedger (ProcessRequest (replica r sys) req)).
+Proof. intros. rewrite NFR_state_nfrValue.
   - induction r.
     + simpl. reflexivity.
-    + simpl. reflexivity.
+    + simpl. remember (GetLedger (ProcessRequest sys req)) as ledger.
+      destruct ledger.
+      { reflexivity. }
+      { reflexivity. }
   - assumption.
 Qed.
 
 Inductive StateValid : State -> Prop :=
-| empty_valid : StateValid nil
+| empty_valid : forall (P:Replica) (n:nat), StateValid (prePrep (P, n))
 | nonfaulty_valid : forall (sys:System) (req:string) (r:Replica) , 
               IsNonFaulty r 
-              -> StateValid (ProcessRequest sys req nil)
-                -> (GetLedger (ProcessRequest sys req nil) nfrValue) 
-                     = (GetLedger (ProcessRequest (r::sys) req nil) nfrValue) 
-                    -> StateValid ((ProcessRequest (r::sys) req nil))
+              -> StateValid (ProcessRequest sys req)
+                -> (GetLedger (ProcessRequest sys req)) 
+                     = (GetLedger (ProcessRequest (replica r sys) req)) 
+                    -> StateValid ((ProcessRequest (replica r sys) req))
 | faulty_valid : forall (sys:System) (req:string) (r:Replica) , ~(IsNonFaulty r) -> 
-      StateValid (ProcessRequest sys req nil) -> StateValid ((ProcessRequest (r::sys) req nil)).
+      StateValid (ProcessRequest sys req) -> StateValid ((ProcessRequest (replica r sys) req)).
 
 Theorem system_safety : forall (sys : System) (m:string),
-      StateValid (ProcessRequest sys m nil).
+      StateValid (ProcessRequest sys m).
 Proof. intros. induction sys.
   - constructor.
-  - induction a.
+  - induction r.
     + apply faulty_valid.
       { unfold not. intros contra. inversion contra. }
       { assumption. }
